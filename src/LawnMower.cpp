e3cc00b1fe36ef832f173e10920626e8f18d2940
Arunava <arunavab713@gmail.com>
2021-12-06 01:17:45 -0500

    [2.5] Create mock L trajectory for testing movebase goals

--- src/LawnMower.cpp
+++ src/LawnMower.cpp
@@ -5,6 +5,7 @@
 
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
+
 // int main(int argc, char** argv){
 //   ros::init(argc, argv, "simple_navigation_goals");
 
@@ -19,14 +20,12 @@ typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseCl
 //   move_base_msgs::MoveBaseGoal goal;
 
 //   //we'll send a goal to the robot to move 1 meter forward
-
-//   goal.target_pose.header.frame_id = "map";
+//   goal.target_pose.header.frame_id = "base_link";
 //   goal.target_pose.header.stamp = ros::Time::now();
 
-//   goal.target_pose.pose.position.x = 0.396480560303;
-//   goal.target_pose.pose.position.y = -0.58054125309;
-//   goal.target_pose.pose.orientation.w = -0.265768806077;
-//   goal.target_pose.pose.orientation.z = 0.964036794794;
+//   goal.target_pose.pose.position.x = 1.0;
+//   goal.target_pose.pose.orientation.w = 1.0;
+
 //   ROS_INFO("Sending goal");
 //   ac.sendGoal(goal);
 
@@ -43,20 +42,26 @@ void LawnMower::mow() {
 
   move_base_msgs::MoveBaseGoal goal;
   MoveBaseClient actionClient("move_base", true);
+  while(!actionClient.waitForServer(ros::Duration(5.0))){
+    ROS_INFO("Waiting for the move_base action server to come up");
+  }
   NavigationUtils navUtils;
   geometry_msgs::Quaternion qMsg;
-  double dummy_theta=0;
-  std::vector<double> dummy_pose = {1,0};
-  qMsg = navUtils.convertToQuaternion(dummy_theta);
-  navUtils.setDesiredGoal(goal, dummy_pose, qMsg);
-  navUtils.sendGoal(goal, actionClient);
-  bool success_flag = navUtils.checkGoalReach(actionClient);
+  std::vector<std::vector<double>> dummy_pos = {{0.5, 0, 0},
+  {0.5, 0, 0}, {0, 0, 90}, {0.5, 0, 0}, {0.5, 0, 0} };
+  for (auto & element : dummy_pos) {
+    qMsg = navUtils.convertToQuaternion(element[2]);
+    navUtils.setDesiredGoal(goal, element, qMsg);
+    ROS_INFO("Sending goal");
+    navUtils.sendGoal(goal, actionClient);
+    bool success_flag = navUtils.checkGoalReach(actionClient);
+  }
 
 }
 LawnMower::LawnMower(ros::NodeHandle n, std::string path) {
   node_h = n;
   path_to_waypoints = path;
-  ros::spin();
+  ros::spinOnce();
 }
 
 int main(int argc, char **argv) {
@@ -66,5 +71,6 @@ int main(int argc, char **argv) {
   ROS_INFO_STREAM("Starting LawnMower... ");
   LawnMower mower(ros_node_h, path);
   mower.mow();
+  ros::spin();
   return 0;
 }
