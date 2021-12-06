2fb872687b696cae51fe852859078133336b58fc
Arunava <arunavab713@gmail.com>
2021-12-06 01:28:21 -0500

    Edited rosinfo output

--- src/NavigationUtils.cpp
+++ src/NavigationUtils.cpp
@@ -16,7 +16,7 @@ void NavigationUtils::setDesiredGoal(move_base_msgs::MoveBaseGoal& goal, std::ve
 
 void NavigationUtils::sendGoal(move_base_msgs::MoveBaseGoal& goal, 
 actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction>& actionClient) {
-  ROS_INFO("Move robot: x= %f, y= %f, yaw= %f", goal.target_pose.pose.position.x, goal.target_pose.pose.position.y, goal.target_pose.pose.orientation);
+  
   actionClient.sendGoal(goal);
 }
 
